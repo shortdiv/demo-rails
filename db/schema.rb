e4ffd9f2da3195f1bfaf773ed418e62375825240
Div <divya@heysparkbox.com>
2014-05-30 17:12:53 -0400

    Added a full_title helper

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508152808) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
